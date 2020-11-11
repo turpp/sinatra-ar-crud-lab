8486924d0ea12c54f60f0e004347cf1c43aa50b6
turpp <white.tripp@icloud.com>
2020-11-11 13:18:29 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,51 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    'whatever'
   end
+
+  get '/articles' do
+    @articles=Article.all
+    erb :index
+  end
+
+  post '/articles' do
+   @article=Article.create(params)
+   redirect "/articles/#{Article.last.id}"
+    erb :show
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id/edit' do
+    
+    @article=Article.find(id=params[:id])
+ 
+    erb :edit
+  end
+
+
+  get '/articles/:id' do
+    @id=params[:id]
+    @article=Article.find(params[:id])
+    erb :show
+  end
+
+  patch '/articles/:id' do
+   
+    @article=Article.find(id=params[:id])
+    @article.update(title: params[:title], content: params[:content])
+    @article.save
+    
+   erb :show
+  end
+
+  delete '/articles/:id' do
+    @article=Article.find(id=params[:id])
+    @article.destroy
+    redirect '/articles'
+  end
+
 end
